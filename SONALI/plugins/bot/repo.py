c99df8d6d7013ae086ea3fb01c2ea4de8f86cbdd
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-19 05:10:48 +0530

    Update repo.py

--- SONALI/plugins/bot/repo.py
+++ SONALI/plugins/bot/repo.py
@@ -9,7 +9,7 @@ start_txt = """
  
 ✰ 𝗣ᴀʜʟᴇ 𝗣ᴀᴘᴀ 𝗕ᴏʟ 𝗥ᴇᴘᴏ 𝗢ᴡɴᴇʀ ᴋᴏ 
 
-✰ || @TheSigmaCoder ||
+✰ || @PerfectseIler ||
  
 ✰ 𝗥ᴜɴ 24x7 𝗟ᴀɢ 𝗙ʀᴇᴇ 𝗪ɪᴛʜᴏᴜᴛ 𝗦ᴛᴏᴘ
  
@@ -22,21 +22,21 @@ async def start(_, msg):
           InlineKeyboardButton("𝗔ᴅᴅ ᴍᴇ 𝗠ᴀʙʏ", url=f"https://t.me/{app.username}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("𝗛ᴇʟᴘ", url="https://t.me/PURVI_UPDATES"),
-          InlineKeyboardButton("⍣ ፝֠֩ ̶ ̶ꭘⷪ ͓ ꯭፝֠֩͠ ̶꯭ ̶͓𝐑꯭α͕υ꯭𝛅͔ʜ꯭α꯭ɴ͓", url="https://t.me/TheSigmaCoder"),
+          InlineKeyboardButton("𝗛ᴇʟᴘ", url="https://t.me/codexempire"),
+          InlineKeyboardButton("𝐈тᷟʑ‌꯭ 🔰 𝚸‌ 𝚵 །⃪᱂ ƒ‌ 𝚵 𝐂 𝛕 ᴵᴰ'ˢ", url="https://t.me/PerfectseIler"),
           ],
                [
-                InlineKeyboardButton("𝗧ᴇᴀᴍ 𝗣ᴜʀᴠɪ 𝗕ᴏᴛs", url=f"https://t.me/PURVI_BOTS"),
+                InlineKeyboardButton("codexempire", url=f"https://t.me/PerfectseIler"),
 ],
 [
-InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/PURVI_MUSIC_ROBOT"),
+InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/MafiyamusicRobot"),
 
         ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://files.catbox.moe/kfg4e6.jpg",
+        photo="https://files.catbox.moe/x8dhjp.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
